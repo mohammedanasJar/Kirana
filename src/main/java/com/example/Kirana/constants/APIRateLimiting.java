6ec8bc30051b7ea51becb54d9b971be877bd087e
Mohammed Anas <mohammedanas@JAR-LT-183.local>
2024-03-26 15:39:09 +0530

    api limit rate added to TransactionAPI

---
+++ src/main/java/com/example/Kirana/constants/APIRateLimiting.java
@@ -0,0 +1,13 @@
+package com.example.Kirana.constants;
+
+import io.github.bucket4j.Bucket;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class APIRateLimiting {
+    public static Map<String, Bucket> transactionBucket=new HashMap<>();
+    public static Map<String, Bucket> reportBucket=new HashMap<>();
+    public static Map<String, Bucket> ratesBucket=new HashMap<>();
+
+}
--- src/main/java/com/example/Kirana/controllers/TransactionAPI.java
+++ src/main/java/com/example/Kirana/controllers/TransactionAPI.java
@@ -1,79 +1,65 @@
 package com.example.Kirana.controllers;
 
 
+import com.example.Kirana.constants.APIRateLimiting;
 import com.example.Kirana.models.TransactionDetails;
-import com.example.Kirana.repos.UserRepo;
 import com.example.Kirana.services.AuthorisationDetails;
 import com.example.Kirana.services.RateLimiter;
 import com.example.Kirana.services.TransactionService;
 import com.example.Kirana.utils.CurrencyConversion;
-import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/transactions")
 public class TransactionAPI {
-
-    @Autowired
-    UserRepo ur;
-    private final Bucket bucket;
-    private Map<String,Bucket> mybucketlist=new HashMap<>();
-
     AuthorisationDetails ad=new AuthorisationDetails();
     @Autowired
     RateLimiter rateLimiter;
     @Autowired
     TransactionService ts;
 
-    public TransactionAPI(){
-        Bandwidth limit=Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
-        this.bucket = Bucket.builder()
-                .addLimit(limit)
-                .build();
-    }
     @Autowired
     CurrencyConversion cc;
     @PostMapping("/record")
     public  ResponseEntity<Object> setTransact(@RequestBody TransactionDetails tt){
-        if(bucket.tryConsume(1)) {
-
-        if(!tt.getCurrencyUsed().equals("USD")){
-            tt.setTransactionAmount(cc.conversion(tt.getCurrencyUsed(),tt.getTransactionAmount()));
+        String username=ad.getUsernameFromAuthorizationHeader();
+        if(APIRateLimiting.transactionBucket.containsKey(username)){
+            Bucket mybucket=APIRateLimiting.transactionBucket.get(username);
+            if (mybucket.tryConsume(1)) {
+                if(!tt.getCurrencyUsed().equals("USD")){
+                    tt.setTransactionAmount(cc.conversion(tt.getCurrencyUsed(),tt.getTransactionAmount()));
+                }
+                ts.setTransaction(tt);
+                return ResponseEntity.ok("Transaction Recorded Successfully\n"+tt.toString());
+            } else {
+                return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+            }
         }
-        ts.setTransaction(tt);
-        return ResponseEntity.ok("test value is "+tt.toString());
+        else{
+            APIRateLimiting.transactionBucket.put(username,rateLimiter.resolveBucket(username));
+            return this.setTransact(tt);
         }
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
-
     }
 
 
     @GetMapping("/getrate")
-    public String getconversion(){
+    public ResponseEntity<Object> getconversion(){
         String username=ad.getUsernameFromAuthorizationHeader();
-        if(mybucketlist.containsKey(username)){
-            Bucket mybucket= mybucketlist.get(username);
+        if(APIRateLimiting.ratesBucket.containsKey(username)){
+            Bucket mybucket=APIRateLimiting.ratesBucket.get(username);
             if (mybucket.tryConsume(1)) {
-                System.out.println(mybucket.getAvailableTokens());
-                return cc.fetchRates();
+                return ResponseEntity.ok(cc.fetchRates());
             } else {
-                return "Rate limit exceeded";
+                return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
             }
-
         }
         else{
-            mybucketlist.put(username,rateLimiter.resolveBucket(username));
+            APIRateLimiting.ratesBucket.put(username,rateLimiter.resolveBucket(username));
+            return getconversion();
         }
-        return "";
-
-   }
+       }
 }
